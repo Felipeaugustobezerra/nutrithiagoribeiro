a84d88ed9ae4e25d92c6907155b0379bb0474614
Felipeaugustobezerra <58082993+Felipeaugustobezerra@users.noreply.github.com>
2026-05-25 22:02:43 +0100

    First commit

---
+++ src/components/sections/hero/index.tsx
@@ -0,0 +1,14 @@
+export function HeroSection() {
+  return (
+    <section className="min-h-screen flex items-center">
+      <div className="container mx-auto">
+        <h1 className="text-6xl font-bold">Seu físico no próximo nível.</h1>
+
+        <p className="mt-6 text-zinc-400 max-w-xl">
+          Consultoria completa para atletas e pessoas que querem alta
+          performance.
+        </p>
+      </div>
+    </section>
+  );
+}
